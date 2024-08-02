e51f21e908a4b90713e2a124348125542ea6f81e
ashoordoushow <ashoor12323@gmail.com>
2024-08-02 11:13:35 -0500

    Add new practice

--- practice.rb
+++ practice.rb
@@ -19,3 +19,10 @@ while number <= 10
   puts number
   number = number + 1
 end
+
+#Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+number = 0
+while number <= 100
+  puts number
+  number = number + 5
+end
