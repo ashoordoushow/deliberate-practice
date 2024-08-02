bd9341b8845ccfc19fa9146811535259ecbf54dd
ashoordoushow <ashoor12323@gmail.com>
2024-08-02 11:04:36 -0500

    practice.rb

---
+++ practice.rb
@@ -0,0 +1,21 @@
+#Write a program that uses a variable to store a number, then prints that number plus 10.
+number = 2
+puts number + 10
+
+
+
+#Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+number = 10
+if number == 10
+  puts 0
+else
+  puts -1
+end
+
+
+#Write a while loop to print the numbers 1 through 10.
+number = 1
+while number <= 10
+  puts number
+  number = number + 1
+end
